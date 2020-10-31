76a1245bed68989f3aee3a3ca8af11fd2f7c913a
Kevin Fung <kevinfung9622@gmail.com>
2020-10-31 15:42:04 -0700

    started the app

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import 'react-native-gesture-handler';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import profile from './assets/profile.png';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={profile} style={styles.profile} />
+      <Text> Open up App.js to start working on your app! 
+                HI
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  profile:{
+    width: 50, 
+    height: 50,
+    position: 'absolute',
+    left: 20,
+    right: 0,
+    top: 30,
+    bottom: 0,
+  }
 });
+
+export default App;
