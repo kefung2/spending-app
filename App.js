4355da8cd333686020c65dfefd24efd3bc2e66f4
Kevin Fung <kevinfung9622@gmail.com>
2020-11-05 20:48:42 -0800

    added input field and button

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import 'react-native-gesture-handler';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 import profile from './assets/profile.png';
+import { TextInput } from "react-native-gesture-handler";
+
+let balence = 10.00;
+//let spending= 0;
+let income = 1000;
 
 function App() {
+  const [spending, setSpending] = useState('')
   return (
     <View style={styles.container}>
+      <TouchableOpacity></TouchableOpacity>
       <Image source={profile} style={styles.profile} />
-      <Text> Open up App.js to start working on your app! 
-      </Text>
+      <Text> ${balence}</Text>
+      <Text> ${spending}</Text>
+      <TextInput 
+        placeholder="I just spended ..."
+        onChangeText={spending => setSpending(spending)}
+        defaultValue={spending}
+      />
+
+      <Button
+        title="Button"
+        onPress={()=> balence = balence - {spending}} // this is not working
+      />
       <StatusBar style="auto" />
     </View>
   );
