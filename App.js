e586ca1ba78b8e640928a126a76d1c42ff4a06a0
Kevin Fung <kevinfung9622@gmail.com>
2020-11-11 15:53:37 -0800

    added a view for each thing, trying to do form right now

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import 'react-native-gesture-handler';
 import { Image, StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
 import profile from './assets/profile.png';
 import { TextInput } from "react-native-gesture-handler";
+import { useForm } from "react-hook-form";
 
 let balence = 10.00;
 //let spending= 0;
@@ -11,10 +12,46 @@ let income = 1000;
 
 function App() {
   const [spending, setSpending] = useState('')
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity></TouchableOpacity>
-      <Image source={profile} style={styles.profile} />
+      {/* View for icon */}
+      <View style={styles.icon}> 
+        <Image source={profile} style={styles.profile}/>
+      </View>
+
+      {/* View for balence */}
+      <View style={styles.balenceView}>
+        <Text style={styles.balenceText}> ${balence}</Text>
+      </View>
+
+      {/* View for input box */}
+      <View style={styles.inboxView}>
+        {/* <TextInput 
+          placeholder="I just spended ..."
+          onChangeText={spending => setSpending(spending)}
+          defaultValue={spending}
+        /> */}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="number" placeholder="I Just Spended ..." name="Spending" ref={register}/>
+
+          <button type="submit"> Submit </button>
+        </form>
+
+      </View>
+
+      {/* View for button */}
+      {/* <View style={styles.buttonView}>
+        <Button
+          title="Button"
+          onPress={()=> balence = balence - {spending}} // this is not working
+        />
+      </View> */}
+
+      {/* <Image source={profile} style={styles.profile} />
       <Text> ${balence}</Text>
       <Text> ${spending}</Text>
       <TextInput 
@@ -26,7 +63,7 @@ function App() {
       <Button
         title="Button"
         onPress={()=> balence = balence - {spending}} // this is not working
-      />
+      /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -38,6 +75,37 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 15,
+  },
+  icon: {
+    flex: 1,
+    width: 24,
+    height: 24,
+    left: 5,
+    top: 5,
+    position: 'absolute',
+  },
+  balenceView: {
+    flex: 1,
+    width: 200,
+    height: 10,
+    top: 50,
+    alignItems: "center",
+  },
+  balenceText:{
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  inboxView: {
+    flex: 1,
+    width: 100,
+    height: 10,
+  },
+  buttonView: {
+    flex: 1,
+    width: 100,
+    height: 10,
+    top: 10,
   },
   profile:{
     width: 50, 
