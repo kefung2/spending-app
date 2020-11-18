517fd223b3c43779a49aa791950a32d032e253cb
Kevin Fung <kevinfung9622@gmail.com>
2020-11-17 23:04:01 -0800

    balence number can now change color, started on Profile page

--- styles/Profilestyle.js
+++ styles/Profilestyle.js
@@ -8,9 +8,35 @@ export default StyleSheet.create({
     justifyContent: "center",
     paddingTop: 15,
   },
-  profile:{
-    width: 60, 
-    height: 60,
+  profileIcon:{
+    flex: 2,
+    width: 300,
     alignSelf:"center",
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  profile:{
+    width: 200, 
+    height: 200,
+    alignSelf:"center",
+    // borderColor: 'black',
+    // borderWidth: 1,
+  },
+  historyButton:{
+    flex: 1,
+    width: 200,
+    alignSelf:"center",
+    justifyContent: 'center',
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  updateButton:{
+    flex: 1,
+    width: 200,
+    alignSelf:"center",
+    justifyContent: 'center',
+    borderColor: 'blue',
+    borderWidth: 1,
   }
 });
